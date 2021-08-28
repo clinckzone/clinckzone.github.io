9b0532080101ecb31eec280b9603ef2b3e02b470
Divyanshu Tiwari <dtiwari@ar.iitr.ac.in>
2021-08-28 19:54:58 +0530

    Edit About Page and Project Descriptions

--- data/articles.js
+++ data/articles.js
@@ -1,26 +1,29 @@
 var ARTICLES = [
-	{	
-		link: "blog/blog-modelling-epidemics",
-		source: "/blog-modelling-epidemics-cover.png",
-		title: "😷 Modelling Epidemics",
-		keywords: ["Math", "Epidemiology"],
-		description: "Due to the ongoing COVID-19 Pandemic, I naturally grew curious to better understand pandemics. Here's my attempt at explaining you this topic.",
+	{
+		link: 'blog/blog-modelling-epidemics',
+		source: '/blog-modelling-epidemics-cover.png',
+		title: '😷 Modelling Epidemics',
+		keywords: ['Math', 'Epidemiology'],
+		description:
+			"Due to the ongoing COVID-19 Pandemic, I naturally grew curious to better understand pandemics. Here's my attempt at explaining you this topic.",
 		date: '25th April 2020',
 	},
 	{
-		link: "/blog/blog-tshirt-data-viz",
-		source: "/blog-tshirt-data-viz-cover.png",
-		title: "👕 T-Shirts and Data Viz",
-		keywords: ["Data", "Design"],
-		description: "Developed a name-based visualization for my T-Shirt design project. I worked over several such visualizations and this article aims to document that.",
+		link: '/blog/blog-tshirt-data-viz',
+		source: '/blog-tshirt-data-viz-cover.png',
+		title: '👕 T-Shirts and Data Viz',
+		keywords: ['Data', 'Design'],
+		description:
+			'Developed a name-based visualization for my T-Shirt design project. I worked over several such visualizations and this article aims to document that.',
 		date: '18th February 2020',
 	},
 	{
-		link: "/blog/blog-razorpay-internship",
-		source: "/blog-razorpay-internship-cover.png",
-		title: "👨‍💻 Razorpay Internship",
-		keywords: ["Internship", "Design",],
-		description: "In the summers of 2019, I interned at Razorpay as a Full-Stack Designer. This blog documents how I made it there and my experience.",
+		link: '/blog/blog-razorpay-internship',
+		source: '/blog-razorpay-internship-cover.png',
+		title: '👨‍💻 Razorpay Internship',
+		keywords: ['Internship', 'Design'],
+		description:
+			'In the summers of 2019, I interned at Razorpay as a Full-Stack Designer. This blog documents how I made it there and my experience thereafter.',
 		date: '30th August 2019',
 	},
 	// {
--- data/projects.js
+++ data/projects.js
@@ -1,46 +1,52 @@
 var PROJECTS = [
 	{
-		link: "/projects/projects-coodle",
+		link: '/projects/projects-coodle',
 		source: 'projects-coodle-cover.png',
-		title: "🃏 Coodle",
-		keywords: ["TUI", "HCI", "Education"],
-		description: "Coodle is an accessible, tangible programming system to help children learn and practice programming in an intuitive and engaging manner."
+		title: '🃏 Coodle',
+		keywords: ['TUI', 'HCI', 'Research', 'Education'],
+		description:
+			'An accessible, tangible programming system to help children learn and practice programming in an intuitive and engaging manner.',
 	},
 	{
-		link: "/projects/projects-two-shapes",
+		link: '/projects/projects-two-shapes',
 		source: 'projects-two-shapes-cover.png',
-		title: "🕹️ Two Shapes",
-		keywords: ["Research", "Games", "HCI"],
-		description: "This project was selected for publication at CHI PLAY 2020 in the Game Design Competition. This post contains the text from the submitted paper."
+		title: '🕹️ Two Shapes',
+		keywords: ['Research', 'Games', 'HCI'],
+		description:
+			'A game designed to teach kids skills of collaboration and empathy. It was selected for publication at the Extended Abstracts of CHI PLAY 2020.',
 	},
 	{
-		link: "/projects/projects-smart-ac",
+		link: '/projects/projects-smart-ac',
 		source: 'projects-smart-ac-cover.png',
-		title: "🎛️ Smart AC Controller",
-		keywords: ["Research", "Energy", "Technology"],
-		description: "Prototype of an automated AC controller that would maintain the AC at a temperature optimised for human comfort and energy efficiency."
+		title: '🎛️ Smart AC Controller',
+		keywords: ['Research', 'Energy', 'Technology'],
+		description:
+			'Prototype of an automated AC controller that would maintain the AC at a temperature optimised for human comfort and energy efficiency.',
 	},
 	{
-		link: "/projects/projects-flappy-rockets",
-		source: "projects-flappy-rockets-cover.png",
-		title: "🚀 Flappy Rockets",
-		keywords: ["Games", "TUI", "HCI"],
-		description: "Flappy rockets is a TUI based asteroid dodging game where you are required to dodge incoming asteroids to protect your spaceship from getting damaged."
+		link: '/projects/projects-flappy-rockets',
+		source: 'projects-flappy-rockets-cover.png',
+		title: '🚀 Flappy Rockets',
+		keywords: ['Games', 'TUI', 'HCI'],
+		description:
+			'Flappy rockets is a TUI based asteroid dodging game where you are required to dodge incoming asteroids to protect your spaceship from getting damaged.',
 	},
 	{
-		link: "/projects/projects-destech-challenge",
-		source: "/projects-destech-challenge-cover.png",
-		title: "🥇 Urban Trails",
-		keywords: ["UX", "Design", "Competition"],
-		description: "A digital platform to capture local urban trails through smartphones to let creatives capture, encapsulate and share their journey in a meaningful way."
+		link: '/projects/projects-destech-challenge',
+		source: '/projects-destech-challenge-cover.png',
+		title: '🥇 Urban Trails',
+		keywords: ['UX', 'Design', 'Competition'],
+		description:
+			'A digital platform to capture local urban trails through smartphones to let creatives capture, encapsulate and share their journey in a meaningful way.',
 	},
 	{
-		link: "/projects/projects-static-site-generator",
-		source: "/projects-static-site-generator-cover.png",
-		title: "💻 Static Site Generator",
-		keywords: ["Web", "UX", "Design"],
-		description: "Static Site Generator is a web application designed to be an easy to use tool to create simple static websites consisting of text and images."
+		link: '/projects/projects-static-site-generator',
+		source: '/projects-static-site-generator-cover.png',
+		title: '💻 Static Site Generator',
+		keywords: ['Web', 'UX', 'Design'],
+		description:
+			'Static Site Generator is a web application designed to be an easy to use tool to create simple static websites consisting of text and images.',
 	},
-]
+];
 
 module.exports = PROJECTS;
