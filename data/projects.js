89857409f6ff22c289335e8fad903c5c669f9a4e
Divyanshu Tiwari <divyanshu.tiwari33@gmail.com>
2023-10-30 18:06:55 +0530

    Fix indentation issues

--- data/projects.js
+++ data/projects.js
@@ -1,68 +1,68 @@
 var PROJECTS = [
-	{
-		link: '/projects/projects-chronicle',
-		source: 'projects-chronicle-cover.png',
-		title: '⭐ Chronicle Endeavors',
-		keywords: ['Frontend', 'Presentation', 'SWE'],
-		description:
-			'An overview of my contributions at Chronicle, a startup reshaping the landscape of presentation creation and delivery.',
-	},
-	{
-		link: '/projects/projects-coodle',
-		source: 'projects-coodle-cover.png',
-		title: '🃏 Coodle',
-		keywords: ['TUI', 'HCI', 'Research', 'Education'],
-		description:
-			'An accessible, tangible programming system to help children learn and practice programming. Selected for publication as poster IndiaHCI 2021',
-	},
-	{
-		link: '/projects/projects-two-shapes',
-		source: 'projects-two-shapes-cover.png',
-		title: '🕹️ Two Shapes',
-		keywords: ['Research', 'Games', 'HCI'],
-		description:
-			'A game designed to teach kids skills of collaboration and empathy. Selected for publication at the Extended Abstracts of CHI PLAY 2020.',
-	},
-	{
-		link: '/projects/projects-navigation-in-ve',
-		source: 'projects-navigation-in-ve-cover.png',
-		title: '⛔ Navigation in VE',
-		keywords: ['Research', 'Psychology'],
-		description:
-			'This research uses a virtual environment (VE) to answer whether route description and map with live updates helps in navigation.',
-	},
-	{
-		link: '/projects/projects-smart-ac',
-		source: 'projects-smart-ac-cover.png',
-		title: '🎛️ Smart AC Controller',
-		keywords: ['Research', 'Energy', 'Technology'],
-		description:
-			'Prototype of an automated AC controller that would maintain the AC at a temperature optimised for human comfort and energy efficiency.',
-	},
-	{
-		link: '/projects/projects-flappy-rockets',
-		source: 'projects-flappy-rockets-cover.png',
-		title: '🚀 Flappy Rockets',
-		keywords: ['Games', 'TUI', 'HCI'],
-		description:
-			'Flappy rockets is a TUI based asteroid dodging game where you are required to dodge incoming asteroids to protect your spaceship from getting damaged.',
-	},
-	{
-		link: '/projects/projects-destech-challenge',
-		source: '/projects-destech-challenge-cover.png',
-		title: '🥇 Urban Trails',
-		keywords: ['UX', 'Design', 'Competition'],
-		description:
-			'A digital platform to capture local urban trails through smartphones to let creatives capture, encapsulate and share their journey in a meaningful way.',
-	},
-	{
-		link: '/projects/projects-static-site-generator',
-		source: '/projects-static-site-generator-cover.png',
-		title: '💻 Static Site Generator',
-		keywords: ['Web', 'UX', 'Design'],
-		description:
-			'Static Site Generator is a web application designed to be an easy to use tool to create simple static websites consisting of text and images.',
-	},
+  {
+    link: '/projects/projects-chronicle',
+    source: 'projects-chronicle-cover.png',
+    title: '⭐ Chronicle Endeavors',
+    keywords: ['Frontend', 'Presentation', 'SWE'],
+    description:
+      'An overview of my contributions at Chronicle, a startup reshaping the landscape of presentation creation and delivery.',
+  },
+  {
+    link: '/projects/projects-coodle',
+    source: 'projects-coodle-cover.png',
+    title: '🃏 Coodle',
+    keywords: ['TUI', 'HCI', 'Research', 'Education'],
+    description:
+      'An accessible, tangible programming system to help children learn and practice programming. Selected for publication as poster IndiaHCI 2021',
+  },
+  {
+    link: '/projects/projects-two-shapes',
+    source: 'projects-two-shapes-cover.png',
+    title: '🕹️ Two Shapes',
+    keywords: ['Research', 'Games', 'HCI'],
+    description:
+      'A game designed to teach kids skills of collaboration and empathy. Selected for publication at the Extended Abstracts of CHI PLAY 2020.',
+  },
+  {
+    link: '/projects/projects-navigation-in-ve',
+    source: 'projects-navigation-in-ve-cover.png',
+    title: '⛔ Navigation in VE',
+    keywords: ['Research', 'Psychology'],
+    description:
+      'This research uses a virtual environment (VE) to answer whether route description and map with live updates helps in navigation.',
+  },
+  {
+    link: '/projects/projects-smart-ac',
+    source: 'projects-smart-ac-cover.png',
+    title: '🎛️ Smart AC Controller',
+    keywords: ['Research', 'Energy', 'Technology'],
+    description:
+      'Prototype of an automated AC controller that would maintain the AC at a temperature optimised for human comfort and energy efficiency.',
+  },
+  {
+    link: '/projects/projects-flappy-rockets',
+    source: 'projects-flappy-rockets-cover.png',
+    title: '🚀 Flappy Rockets',
+    keywords: ['Games', 'TUI', 'HCI'],
+    description:
+      'Flappy rockets is a TUI based asteroid dodging game where you are required to dodge incoming asteroids to protect your spaceship from getting damaged.',
+  },
+  {
+    link: '/projects/projects-destech-challenge',
+    source: '/projects-destech-challenge-cover.png',
+    title: '🥇 Urban Trails',
+    keywords: ['UX', 'Design', 'Competition'],
+    description:
+      'A digital platform to capture local urban trails through smartphones to let creatives capture, encapsulate and share their journey in a meaningful way.',
+  },
+  {
+    link: '/projects/projects-static-site-generator',
+    source: '/projects-static-site-generator-cover.png',
+    title: '💻 Static Site Generator',
+    keywords: ['Web', 'UX', 'Design'],
+    description:
+      'Static Site Generator is a web application designed to be an easy to use tool to create simple static websites consisting of text and images.',
+  },
 ];
 
 module.exports = PROJECTS;
--- server.js
+++ server.js
@@ -11,62 +11,62 @@ server.use(express.static(__dirname + '/scripts'));
 server.use(express.static(__dirname + '/data'));
 
 server.get('/', function (req, res) {
-	res.render('index', {
-		title: 'Home',
-		stylesheet: 'index.css',
-		projects: PROJECTS,
-		articles: ARTICLES,
-	});
+  res.render('index', {
+    title: 'Home',
+    stylesheet: 'index.css',
+    projects: PROJECTS,
+    articles: ARTICLES,
+  });
 });
 
 server.get('/about', function (req, res) {
-	res.render('about', {
-		title: 'About',
-		stylesheet: 'page.css',
-	});
+  res.render('about', {
+    title: 'About',
+    stylesheet: 'page.css',
+  });
 });
 
 server.get('/blog', function (req, res) {
-	res.render('blog', {
-		title: 'Blog',
-		stylesheet: 'projects.css',
-		articles: ARTICLES,
-	});
+  res.render('blog', {
+    title: 'Blog',
+    stylesheet: 'projects.css',
+    articles: ARTICLES,
+  });
 });
 
 server.get('/blog/:blogPost', function (req, res) {
-	const blogPost = req.params.blogPost;
-	const blogTitle = ARTICLES.find((article) => article.link === `/blog/${blogPost}`).title;
-	res.render(`${blogPost}`, {
-		title: blogTitle,
-		stylesheet: '../page.css',
-	});
+  const blogPost = req.params.blogPost;
+  const blogTitle = ARTICLES.find((article) => article.link === `/blog/${blogPost}`).title;
+  res.render(`${blogPost}`, {
+    title: blogTitle,
+    stylesheet: '../page.css',
+  });
 });
 
 server.get('/projects', function (req, res) {
-	res.render('projects', {
-		title: 'Projects',
-		stylesheet: 'projects.css',
-		projects: PROJECTS,
-	});
+  res.render('projects', {
+    title: 'Projects',
+    stylesheet: 'projects.css',
+    projects: PROJECTS,
+  });
 });
 
 server.get('/projects/:projectPost', function (req, res) {
-	const projectPost = req.params.projectPost;
-	const projectTitle = PROJECTS.find(
-		(project) => project.link === `/projects/${projectPost}`
-	).title;
-	res.render(`${projectPost}`, {
-		title: projectTitle,
-		stylesheet: '../page.css',
-	});
+  const projectPost = req.params.projectPost;
+  const projectTitle = PROJECTS.find(
+    (project) => project.link === `/projects/${projectPost}`
+  ).title;
+  res.render(`${projectPost}`, {
+    title: projectTitle,
+    stylesheet: '../page.css',
+  });
 });
 
 let port = process.env.PORT;
 if (port == null || port == '') {
-	port = 8000;
+  port = 8000;
 }
 
 server.listen(port, function () {
-	console.log('Server is running on ' + port);
+  console.log('Server is running on ' + port);
 });
